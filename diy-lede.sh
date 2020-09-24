26b521a4f083340b633000faf6956731c8787cff
svgr110 <70746911+svgr110@users.noreply.github.com>
2020-09-24 10:23:08 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -6,7 +6,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.2.2/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' ./package/base-files/files/bin/config_generate
 
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
@@ -39,7 +39,7 @@ git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-ap
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan  #微信推送
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
 git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
-git clone https://github.com/cnzd/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
+git clone https://github.com/prcxu/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
